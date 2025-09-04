5c560d961f5d9eb3247a09dd31c9a3074be11d39
Jonas Berger Nyvold <jonas.nyvold02@gmail.com>
2025-09-04 23:18:58 +0200

    adds country and population, needs fail message to user if countri invalid, and search feature

--- assignment-1/main.js
+++ assignment-1/main.js
@@ -1,20 +1,4 @@
-const newLi = (input) => { // 1
-    const list = document.getElementById("currency-list")
-    const inputField = document.getElementById("currency")
-    
-    if (input.trim() !== "") {
-        const li = document.createElement("li")
-
-        li.textContent = input
-
-        const delButton = newDelButton(() => list.removeChild(li))
-        li.appendChild(delButton)
-
-        list.appendChild(li)
-        inputField.value = ""
-    }
-}
-
+// helpers
 const newDelButton = (action) => { // 2
     const delButton = document.createElement("button")
         delButton.textContent = "X"
@@ -31,9 +15,83 @@ const checkPrefix = (list, searchWord) => { // 3B
     return list.filter(item => startsWithPrefix(item, searchWord))
 }
 
-document.getElementById("button").addEventListener("click", () => {
+// country list
+let validCountries = []
+window.addEventListener("DOMContentLoaded", async () => { // 5
+    const response = await fetch('https://d6wn6bmjj722w.population.io/1.0/countries/')
+    if(!response.ok) {throw new Error(`response unsuccessful with code: ${response.status}`)}
+    
+    data = await response.json()
+    validCountries = data.countries
+
+    console.log(validCountries)
+})
+
+const fetchPopulation = async (country) => {
+    const response = await fetch(`https://d6wn6bmjj722w.population.io/1.0/population/${country}/today-and-tomorrow/`)
+    if(!response.ok) {throw new Error(`response unsuccessful with code: ${response.status}`)}
+
+    data = await response.json()
+    population = data.total_population[0].population
+    console.log(population)
+    return population
+    
+}
+
+const newCountryLi = async (country) => { // 5
+    const list = document.getElementById("country-list")
+    const inputField = document.getElementById("country")
+    
+    if (country.trim() !== "" && validCountries.includes(country)) {
+        const population = await fetchPopulation(country)
+        const li = document.createElement("li")
+
+        li.textContent = `${country} - ${population}`
+
+        const delButton = newDelButton(() => list.removeChild(li))
+        li.appendChild(delButton)
+
+        list.appendChild(li)
+        inputField.value = ""
+    }
+}
+
+document.getElementById("country-button").addEventListener("click", () => {
         const inputField = document.getElementById("currency")
-        newLi(inputField.value)
+        newCountryLi(inputField.value)
+    }
+)
+
+document.getElementById("country").addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault()
+            const inputField = document.getElementById("country")
+            newCountryLi(inputField.value)
+        }
+    }
+)
+
+// currency list
+const newCurrencyLi = (currency) => { // 1
+    const list = document.getElementById("currency-list")
+    const inputField = document.getElementById("currency")
+    
+    if (currency.trim() !== "") {
+        const li = document.createElement("li")
+
+        li.textContent = currency
+
+        const delButton = newDelButton(() => list.removeChild(li))
+        li.appendChild(delButton)
+
+        list.appendChild(li)
+        inputField.value = ""
+    }
+}
+
+document.getElementById("currency-button").addEventListener("click", () => {
+        const inputField = document.getElementById("currency")
+        newCurrencyLi(inputField.value)
     }
 )
 
@@ -41,15 +99,15 @@ document.getElementById("currency").addEventListener("keypress", (event) => {
         if (event.key === "Enter") {
             event.preventDefault()
             const inputField = document.getElementById("currency")
-            newLi(inputField.value)
+            newCurrencyLi(inputField.value)
         }
     }
 )
 
-document.getElementById("search").addEventListener("input", (event) => {
+document.getElementById("currency-search").addEventListener("input", (event) => {
         const list = document.getElementById("currency-list")
         const listItems = Array.from(list.getElementsByTagName("li"))
-        const searchWord = document.getElementById("search").value
+        const searchWord = document.getElementById("currency-search").value
         if (searchWord === "") {
             listItems.forEach(item => (item.style.display = ""))    
         } else {
