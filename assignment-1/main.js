77c0d6a060de3552ee2894656650ecd9cd6c13c1
Jonas Berger Nyvold <jonas.nyvold02@gmail.com>
2025-09-16 19:39:07 +0200

    oblig 1

--- assignment-1/main.js
+++ assignment-1/main.js
@@ -1,5 +1,37 @@
-// helpers
-const newDelButton = (action) => { // 2
+// step 1
+const newCurrencyLi = (currency) => { 
+    const list = document.getElementById("currency-list")
+    const inputField = document.getElementById("currency")
+    
+    if (currency.trim() !== "") {
+        const li = document.createElement("li")
+
+        li.textContent = currency
+
+        const delButton = newDelButton(() => list.removeChild(li))
+        li.appendChild(delButton)
+
+        list.appendChild(li)
+        inputField.value = ""
+    }
+}
+
+document.getElementById("currency-button").addEventListener("click", () => {
+        const inputField = document.getElementById("currency")
+        newCurrencyLi(inputField.value)
+    }
+)
+
+document.getElementById("currency").addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            const inputField = document.getElementById("currency")
+            newCurrencyLi(inputField.value)
+        }
+    }
+)
+
+// step 2
+const newDelButton = (action) => {
     const delButton = document.createElement("button")
         delButton.textContent = "X"
         delButton.classList.add("delButton")
@@ -7,17 +39,33 @@ const newDelButton = (action) => { // 2
         return delButton
 }
 
+// step 3
 const startsWithPrefix = (element, searchWord) => { // 3A
     return element.textContent.toLowerCase().startsWith(searchWord.toLowerCase())
 }
 
-const checkPrefix = (list, searchWord) => { // 3B
+const filterByPrefix = (list, searchWord) => { // 3B
     return list.filter(item => startsWithPrefix(item, searchWord))
 }
 
-// country list
+// step 4
+document.getElementById("currency-search").addEventListener("input", (event) => {
+        const list = document.getElementById("currency-list")
+        const listItems = Array.from(list.getElementsByTagName("li"))
+        const searchWord = document.getElementById("currency-search").value
+        if (searchWord === "") {
+            listItems.forEach(item => (item.style.display = ""))    
+        } else {
+            match = filterByPrefix(listItems, searchWord)
+            listItems.forEach(item => (item.style.display = "none"))
+            match.forEach(item => (item.style.display = ""))
+        }
+    }
+)
+
+// step 5
 let validCountries = []
-window.addEventListener("DOMContentLoaded", async () => { // 5
+window.addEventListener("DOMContentLoaded", async () => { 
     const response = await fetch('https://d6wn6bmjj722w.population.io/1.0/countries/')
     if(!response.ok) {throw new Error(`response unsuccessful with code: ${response.status}`)}
     
@@ -38,7 +86,7 @@ const fetchPopulation = async (country) => {
     
 }
 
-const newCountryLi = async (country) => { // 5
+const newCountryLi = async (country) => {
     const list = document.getElementById("country-list")
     const inputField = document.getElementById("country")
     const errorDiv = document.getElementById("country-error")
@@ -59,7 +107,6 @@ const newCountryLi = async (country) => { // 5
         errorDiv.textContent = `${country} is not a supported country`
         errorDiv.style.visibility = "visible"
         setTimeout(() => {
-            errorDiv.textContent = ""
             errorDiv.style.visibility = "hidden"
         }, 4000)
     }
@@ -87,55 +134,7 @@ document.getElementById("country-search").addEventListener("input", (event) => {
         if (searchWord === "") {
             listItems.forEach(item => (item.style.display = ""))    
         } else {
-            match = checkPrefix(listItems, searchWord)
-            listItems.forEach(item => (item.style.display = "none"))
-            match.forEach(item => (item.style.display = ""))
-        }
-        
-    }
-)
-
-// currency list
-const newCurrencyLi = (currency) => { // 1
-    const list = document.getElementById("currency-list")
-    const inputField = document.getElementById("currency")
-    
-    if (currency.trim() !== "") {
-        const li = document.createElement("li")
-
-        li.textContent = currency
-
-        const delButton = newDelButton(() => list.removeChild(li))
-        li.appendChild(delButton)
-
-        list.appendChild(li)
-        inputField.value = ""
-    }
-}
-
-document.getElementById("currency-button").addEventListener("click", () => {
-        const inputField = document.getElementById("currency")
-        newCurrencyLi(inputField.value)
-    }
-)
-
-document.getElementById("currency").addEventListener("keypress", (event) => {
-        if (event.key === "Enter") {
-            event.preventDefault()
-            const inputField = document.getElementById("currency")
-            newCurrencyLi(inputField.value)
-        }
-    }
-)
-
-document.getElementById("currency-search").addEventListener("input", (event) => {
-        const list = document.getElementById("currency-list")
-        const listItems = Array.from(list.getElementsByTagName("li"))
-        const searchWord = document.getElementById("currency-search").value
-        if (searchWord === "") {
-            listItems.forEach(item => (item.style.display = ""))    
-        } else {
-            match = checkPrefix(listItems, searchWord)
+            match = filterByPrefix(listItems, searchWord)
             listItems.forEach(item => (item.style.display = "none"))
             match.forEach(item => (item.style.display = ""))
         }
